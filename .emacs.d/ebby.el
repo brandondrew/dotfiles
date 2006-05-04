2ba31cad9bcbc44828bd16a016716289ab7fa798
phil <phil@bf57b59b-9afd-0310-b0be-d7949ef07b0b>
2006-05-04 22:35:37 +0000

    renamed eobby to ebby
    git-svn-id: file:///var/svn/dotfiles@201 bf57b59b-9afd-0310-b0be-d7949ef07b0b

--- .emacs.d/ebby.el
+++ .emacs.d/ebby.el
@@ -2,7 +2,7 @@
 ;; Copyright (C) 2006 Phil Hagelberg
 
 ;; Author: Phil Hagelberg
-;; URL: http://dev.technomancy.us/phil/wiki/eobby
+;; URL: http://dev.technomancy.us/phil/wiki/ebby
 
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -28,21 +28,21 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Init
 
-(defgroup eobby nil
+(defgroup ebby nil
   "Implementation of the obby collaborative editing protocol"
   :version "22.1" ; not sure what to put here...
-  :prefix "eobby-"
+  :prefix "ebby-"
   :group 'applications)
 
-(defcustom eobby-default-port 6522
+(defcustom ebby-default-port 6522
   "Default port to connect to."
   :type 'integer
-  :group 'eobby)
+  :group 'ebby)
 
-(defcustom eobby-default-name (user-login-name)
+(defcustom ebby-default-name (user-login-name)
   "Your user name."
   :type 'string
-  :group 'eobby)
+  :group 'ebby)
 
 (setq client-table (make-hash-table :test 'equal)) ; clients referenced by net6-user-id
 
@@ -54,15 +54,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Connection:
 
-(defun eobby-connect (server name color &optional port)
+(defun ebby-connect (server name color &optional port)
   (save-excursion
     (message "Connecting to %s..." server)
     (let* ((port-number (if port
 			    (if (stringp port)
 				(string-to-number port)
 			      port)
-			  eobby-default-port))
-	   (name (or name eobby-default-name))
+			  ebby-default-port))
+	   (name (or name ebby-default-name))
            (process (open-network-stream server nil server port-number)))
 
       (setq *user-name* name)
@@ -70,47 +70,47 @@
       
       ;; set up process
       (set-process-coding-system process 'raw-text 'raw-text)
-      (set-process-filter process 'eobby-filter)
+      (set-process-filter process 'ebby-filter)
 
       ;; associate with a buffer?
-      (switch-to-buffer "eobby")
+      (switch-to-buffer "ebby")
       (set-process-buffer process (current-buffer))
 
       ;; log in
-      (eobby-send-string (concat "net6_client_login:" name ":" color)))))
+      (ebby-send-string (concat "net6_client_login:" name ":" color)))))
 
-(defun eobby-filter (process output)
+(defun ebby-filter (process output)
   (setq lines (split-string output "\n"))
-  (set-buffer "eobby")
+  (set-buffer "ebby")
   (insert output)
   (dolist (line lines)
-    (eobby-filter-line process line)))
+    (ebby-filter-line process line)))
 
-(defun eobby-filter-line (process line)
+(defun ebby-filter-line (process line)
   (setq tokens (split-string line ":"))
   (cond
-    ((equal (car tokens) "obby_welcome") (apply 'eobby-welcome (cdr tokens)))
+    ((equal (car tokens) "obby_welcome") (apply 'ebby-welcome (cdr tokens)))
 ;    ((equal (car tokens) "obby_sync_init")) ; well? what's it for?
 ;    ((equal (car tokens) "obby_sync_final")) ; right...
-    ((equal (car tokens) "net6_client_join") (apply 'eobby-client-join (cdr tokens)))
-    ((equal (car tokens) "net6_client_part") (apply 'eobby-client-part (cdr tokens)))
-    ((equal (car tokens) "obby_sync_doclist_document") (apply 'eobby-synch-doclist-document (cdr tokens)))
-    ((equal (car tokens) "obby_document_create") (apply 'eobby-document-create (cdr tokens)))
-    ((equal (car tokens) "obby_document") (apply 'eobby-document-handler (cdr tokens)))
+    ((equal (car tokens) "net6_client_join") (apply 'ebby-client-join (cdr tokens)))
+    ((equal (car tokens) "net6_client_part") (apply 'ebby-client-part (cdr tokens)))
+    ((equal (car tokens) "obby_sync_doclist_document") (apply 'ebby-synch-doclist-document (cdr tokens)))
+    ((equal (car tokens) "obby_document_create") (apply 'ebby-document-create (cdr tokens)))
+    ((equal (car tokens) "obby_document") (apply 'ebby-document-handler (cdr tokens)))
     ;; more functions here
     ))
 
-(defun eobby-send-string (string &optional process)
-  (unless process (setq process (get-buffer-process "eobby")))
+(defun ebby-send-string (string &optional process)
+  (unless process (setq process (get-buffer-process "ebby")))
   (unless (eq (process-status process) 'open)
     (error "Network connection to %s is not open"
 	   (process-name process)))
   (process-send-string process (concat string "\n")))
 
-(defun eobby-subscribe (author document)
-  (eobby-send-string (concat "obby_document:" (get-document-id document) ":subscribe:" *user-id*)))
+(defun ebby-subscribe (author document)
+  (ebby-send-string (concat "obby_document:" (get-document-id document) ":subscribe:" *user-id*)))
 
-(defun eobby-unsubscribe (doc-id)
+(defun ebby-unsubscribe (doc-id)
   ;; TODO
 )
 
@@ -118,57 +118,57 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; General Handlers
 
-(defun eobby-welcome (protocol-version &rest args)
+(defun ebby-welcome (protocol-version &rest args)
   ;; TODO: should throw an error if the protocol version is wrong
   )
 
-(defun eobby-client-join (net6-user-id name obby-user-id color)
+(defun ebby-client-join (net6-user-id name obby-user-id color)
   ;; add client to client-table
   (if (equal name *user-name*)
       (setq *user-id* obby-user-id)) ; only chance at getting our own user ID
   (puthash net6-user-id '(:net6-id net6-user-id :name name :obby-id obby-user-id :color color) client-table))
 
-(defun eobby-client-part (net6-user-id)
+(defun ebby-client-part (net6-user-id)
   ;; drop client from client-table
   (remhash net6-user-id client-table))
 
-(defun eobby-synch-doclist-document (obby-user-id doc-index doc-name &rest users)
+(defun ebby-synch-doclist-document (obby-user-id doc-index doc-name &rest users)
   ;; add document to document-table
   ;; TODO: add users to document
-  (eobby-document-create obby-user-id doc-index doc-name))
+  (ebby-document-create obby-user-id doc-index doc-name))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Document Handlers
 
-(defun eobby-document-handler (doc-id command &rest args)
+(defun ebby-document-handler (doc-id command &rest args)
   (setq blah args)
   (cond
-    ((equal command "sync_init") (apply 'eobby-document-sync-init doc-id args))
-    ((equal command "sync_line") (apply 'eobby-document-sync-line doc-id args))
-    ((equal command "subscribe") (apply 'eobby-document-subscribe doc-id))
-    ((equal command "record") (apply 'eobby-document-record doc-id args))
+    ((equal command "sync_init") (apply 'ebby-document-sync-init doc-id args))
+    ((equal command "sync_line") (apply 'ebby-document-sync-line doc-id args))
+    ((equal command "subscribe") (apply 'ebby-document-subscribe doc-id))
+    ((equal command "record") (apply 'ebby-document-record doc-id args))
     ;; TODO determine complete list of commands, write case-string
 ))
 
-(defun eobby-document-sync-init (&rest args)
+(defun ebby-document-sync-init (&rest args)
   ;; TODO write? do we care about this at all?
 )
 
-(defun eobby-document-sync-line (&rest args)
+(defun ebby-document-sync-line (&rest args)
   ;; TODO write
   ;; necessary for subscribing to documents that already have content.
 )
 
-(defun eobby-document-create (doc-owner-id doc-count doc-name)
+(defun ebby-document-create (doc-owner-id doc-count doc-name)
   "Add document to document-table"
   (puthash (concat doc-owner-id " " doc-count) ; key
 	   '(:owner doc-owner-id :index doc-count :name doc-name :users ())
 	   document-table))
 
-(defun eobby-document-subscribe (doc-id)
+(defun ebby-document-subscribe (doc-id)
   (setq document (gethash "1 1" document-table))
 
-  (switch-to-buffer (concat "eobby-" (getf document :name)))
+  (switch-to-buffer (concat "ebby-" (getf document :name)))
 
   ;; not sure what we do with these, but it's good to store them, i guess
   (make-local-variable 'owner-id)
@@ -185,20 +185,20 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Document Record Handlers
 
-(defun eobby-document-record (doc-id user-id version zero command &rest args)
+(defun ebby-document-record (doc-id user-id version zero command &rest args)
   (cond
-    ((equal "ins" command) (apply 'eobby-document-record-ins doc-id args))
-    ((equal "del" command) (apply 'eobby-document-record-del doc-id args))
+    ((equal "ins" command) (apply 'ebby-document-record-ins doc-id args))
+    ((equal "del" command) (apply 'ebby-document-record-del doc-id args))
     ;; TODO: determine if there are more functions
     ))
 
-(defun eobby-document-record-ins (doc-id position char)
+(defun ebby-document-record-ins (doc-id position char)
   (save-excursion
     (set-buffer (doc-id-to-buffer doc-id))
     (goto-char (+ (string-to-number position 16) 1)) ; obby starts at zero
-    (insert (eobby-unescape char))))
+    (insert (ebby-unescape char))))
 
-(defun eobby-document-record-del (doc-id position char-count)
+(defun ebby-document-record-del (doc-id position char-count)
   (save-excursion
     (set-buffer (doc-id-to-buffer doc-id))
     (goto-char (+ (string-to-number position 16) 1)) ; obby starts at zero
@@ -215,12 +215,12 @@
 (defun doc-id-to-buffer (doc-id)
   (getf (gethash doc-id document-table) :name))
 
-(defun eobby-unescape (char)
+(defun ebby-unescape (char)
   (cond ((equal char "\\n") "\n")
 	((equal char "\\d") ":")
 	(t char)))
 
-(provide 'eobby)
+(provide 'ebby)
 
-;(eobby-connect "192.168.1.44" "eobby-test" "ff0000" 6524)
-;(eobby-send-string "obby_document:1 1:subscribe:2")
+;(ebby-connect "192.168.1.44" "ebby-test" "ff0000" 6524)
+;(ebby-send-string "obby_document:1 1:subscribe:2")
