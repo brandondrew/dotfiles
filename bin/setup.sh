43ecb4ee52fcbd4742609629a7b80f5b02caadb4
philhag <philhag@bf57b59b-9afd-0310-b0be-d7949ef07b0b>
2007-05-29 15:16:31 +0000

    added to installation process
    git-svn-id: file:///var/svn/dotfiles@326 bf57b59b-9afd-0310-b0be-d7949ef07b0b

--- bin/setup.sh
+++ bin/setup.sh
@@ -11,7 +11,7 @@ fi
 
 apt-get update
 
-apt-get install epiphany-browser epiphany-extensions emacs21 subversion ruby1.8-dev sqlite3 libsqlite3-dev f-spot gmpc x2x gaim-otr gaim-encryption gaim-hotkeys synergy libsqlite3-ruby1.8 abiword gnumeric avahi-daemon bittorrent sbcl armagetron ekiga build-essential graphviz sshfs openssh-server mpg321 netcat nmap popularity-contest sawfish wmii rxvt-unicode sawfish-themes screen snes9x-x swig tidy unison unzip visualboyadvance w3m-img wmctrl workrave wxvlc xmms cvs xfonts-terminus namazu fetchmail libopenssl-ruby1.8 xulrunner zile mysql-server libmysql-ruby1.8 rdoc1.8 ri1.8 libnotify-bin ruby-gnome2 libgnome2-ruby rubygems mplayer konqueror k3b lame gstreamer0.10-plugins-ugly-multiverse gstreamer0.10-fluendo-mp3 libgnome2-ruby ruby-gnome2 svk
+apt-get install epiphany-browser epiphany-extensions emacs21 subversion ruby1.8-dev sqlite3 libsqlite3-dev f-spot gmpc x2x gaim-otr gaim-encryption gaim-hotkeys synergy libsqlite3-ruby1.8 abiword gnumeric avahi-daemon bittorrent sbcl armagetron ekiga build-essential graphviz sshfs openssh-server mpg321 netcat nmap popularity-contest sawfish wmii rxvt-unicode sawfish-themes screen snes9x-x swig tidy unison unzip visualboyadvance w3m-img wmctrl workrave wxvlc xmms cvs xfonts-terminus namazu fetchmail libopenssl-ruby1.8 xulrunner zile mysql-server libmysql-ruby1.8 rdoc1.8 ri1.8 libnotify-bin ruby-gnome2 libgnome2-ruby mplayer konqueror k3b lame gstreamer0.10-plugins-ugly-multiverse gstreamer0.10-fluendo-mp3 libgnome2-ruby ruby-gnome2 svk service-discovery-applet avahi-discover avahi-utils
 
 sed s/DisallowTCP=true/DisallowTCP=false/ /etc/gdm/gdm.conf.old > /etc/gdm/gdm.conf
 
