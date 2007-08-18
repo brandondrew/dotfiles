88db4fd6f134d423fce3c461d9be38d1c2a25925
phil <phil@bf57b59b-9afd-0310-b0be-d7949ef07b0b>
2007-08-18 19:57:49 +0000

    ip check fix
    git-svn-id: file:///var/svn/dotfiles@362 bf57b59b-9afd-0310-b0be-d7949ef07b0b

--- bin/ip_check.rb
+++ bin/ip_check.rb
@@ -2,15 +2,15 @@
 # ruby really *is* the new perl!
 
 def current_ip
-  router_password = File.read(File.expand_path('~/.router_password'))
-  `curl -# -u  admin:#{router_password} http://192.168.0.1/cgi-bin/webcm?getpage=..%2Fhtml%2Findex_real.html\\&var%3Aconname=connection0\\&var%3Acontype=pppoa 2>&1 /dev/null`.match(/(\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?)/).to_s
+  router_password = File.read(File.expand_path('~/.router_password')).strip
+  `curl -u admin:#{router_password} http://192.168.0.1/cgi-bin/webcm?getpage=..%2Fhtml%2Findex_real.html\\&var%3Aconname=connection0\\&var%3Acontype=pppoa 2>&1 /dev/null`.match(/(\d\d?\d?\.\d\d?\d?\.\d\d?\d?\.\d\d?\d?)/).to_s
 end
 
 `touch ~/.ip`
 
-if File.read(File.expand_path('~/.ip')) != current_ip
-  # IP has changed!
-  File.open(File.expand_path('~/.new_ip')){ |f| f.puts current_ip }
-  `scp ~/.new_ip philhag@hagelb.org`
+if File.read(File.expand_path('~/.ip')).strip != current_ip
+  puts "IP has changed!"
+  File.open(File.expand_path('~/.new_ip'), 'w'){ |f| f.puts current_ip }
+  `scp ~/.new_ip philhag@hagelb.org:`
   `mv ~/.new_ip ~/.ip`
 end
