b92114042c6e21fafa953a07ba79334f2045574a
Phil Hagelberg <phil@imp.(none)>
2007-10-23 11:02:31 -0700

    gnus searching with namazu

--- .emacs.d/ruby.el
+++ .emacs.d/ruby.el
@@ -38,6 +38,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode)) ; d'oh!
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.erb$" . rhtml-mode))
 (add-to-list 'auto-mode-alist '("\\.builder$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.mab$" . ruby-mode))
--- .gnus.el
+++ .gnus.el
@@ -1,13 +1,16 @@
+;; General config
 (setq gnus-select-method '(nnnil ""))
 (setq gnus-secondary-select-methods '((nnml ""
 					    (nnml-directory "~/Mail"))))
-
 (setq gnus-home-directory "~/.emacs.d")
+
+;; Sending mail details
 (setq user-mail-address "phil@evri.com")
 (setq user-full-name "Phil Hagelberg")
 (setq gnus-ignored-from-addresses "Phil Hagelberg")
+(setq gnus-message-archive-group "sent")
 
-;; Now with SSL!
+;; SMTP
 (setq smtpmail-starttls-credentials '(("mail.hagelb.org" 587 nil nil))
       smtpmail-smtp-server "mail.hagelb.org"
       smtpmail-default-smtp-server "mail.hagelb.org"
@@ -19,10 +22,10 @@
 				   "m7139145"
 				   "testyy")))
 
-(setq message-kill-buffer-on-exit t)
+;; Incoming mail
+(setq mail-sources '((file :path "/var/mail/phil")))
 
-(setq gnus-message-archive-group "sent")
-(setq gnus-fetch-old-headers 'some)
+;; Sorting
 (setq nnmail-split-methods 'nnmail-split-fancy)
 (setq nnmail-crosspost nil)
 
@@ -32,48 +35,50 @@
 	  (from "zabbix" "zabbix")
 	  (from "infact" "infact")
 	  (any "articles@" "articles")
+	  (to "phil@localhost" "feeds")
 	  (from "xplanner" junk)
 	  (from "Apple" junk)
 	  "inbox"))
 
-(setq mail-sources '((file :path "/var/mail/phil")))
+;; Misc settings
+(setq message-kill-buffer-on-exit t
+      gnus-fetch-old-headers 'some
+      gnus-agent-expire-days 0
+      gnus-agent-enable-expiration 'DISABLE
+      gnus-use-full-window nil
+      mm-text-html-renderer 'w3m)
 
-(setq imap-ssl-program "/usr/bin/openssl s_client -ssl3 -connect %s:%p")
-(setq gnus-agent-expire-days 0)
-(setq gnus-agent-enable-expiration 'DISABLE)
-
-(gnus-demon-add-handler 'gnus-group-get-new-news 10 t)
-(gnus-demon-init)
-
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
 (add-hook 'message-mode-hook 'auto-fill-mode)
 (add-hook 'message-mode-hook 'flyspell-mode)
 
-(setq gnus-summary-line-format
-      (concat
-       "%{|%}"
-       "%U%R%z"
-       "%{|%}"
-       "%(%-18,18f"
-       "%{|%}"
-       "%*%{%B%} %s%)"
-       "\n"))
-
-(when (require 'nnir nil t)
-  (setq nnir-search-engine 'namazu)
-  (setq nnir-namazu-index-directory
-        (expand-file-name "~/.namazu"))
-  (setq nnir-namazu-remove-prefix
-        (expand-file-name "~/Mail/"))
-  (setq nnir-mail-backend (nth 0 gnus-secondary-select-methods)))
-
-(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
-(setq gnus-use-full-window nil)
-
 (mailcap-add "image/jpeg" "display")
 
-;; (setq gnus-buffer-configuration 
-;;       '((group (vertical 1.0 (group 1.0 point)))
-;; 	(summary (vertical 1.0 (summary 1.0 point)))
-;; 	(article (horizontal 1.0 (summary 1.0 point)
-;; 			   (article 80)))))
+;; Cosmetics
+(setq gnus-summary-line-format "%O%U%R%z%d %B%(%[%4L: %-22,22f%]%) %s\n")
+(setq gnus-summary-mode-line-format "Gnus: %p [%A / Sc:%4z] %Z")
 
+(setq gnus-summary-same-subject "")
+(setq gnus-sum-thread-tree-root "")
+(setq gnus-sum-thread-tree-single-indent "")
+(setq gnus-sum-thread-tree-leaf-with-other "+-> ")
+(setq gnus-sum-thread-tree-vertical "|")
+(setq gnus-sum-thread-tree-single-leaf "`-> ")
+
+
+;; Searching
+(require 'gnus-namazu)
+(gnus-namazu-insinuate)
+(setq gnus-namazu-index-directories
+      (list (expand-file-name "~/.cache/namazu")))
+
+
+;; auto-check
+(gnus-demon-add-handler 'gnus-group-get-new-news 10 t)
+(gnus-demon-init)
+
+;; TODO:
+;; - ical integration: http://bc.tech.coop/blog/070306.html and https://zimbra.evri.com/home/phil/Calendar/
+;; - what is gnu-registry?
+;; - bbdb?
+;; - sort group by
