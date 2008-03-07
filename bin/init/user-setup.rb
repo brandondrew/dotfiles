3e04b35dd03f34c39db4c5911340412ae2fcdecd
Phil Hagelberg <phil@imp.(none)>
2008-03-07 09:52:41 -0800

    removed emacs-w3m from .emacs.d; compile it separately

--- bin/init/user-setup.rb
+++ bin/init/user-setup.rb
@@ -1,10 +1,9 @@
-system "cd"
-system "mv dotfiles/* ~"
-system "mv dotfiles/.* ~"
+system "mv ~/dotfiles/* ~"
+system "mv ~/dotfiles/.* ~"
 
-system "cp .sshconfig .ssh/config"
+system "cp ~/.sshconfig ~/.ssh/config"
 system "ln -s /usr/lib/ruby/gems/1.8/gems ~/gems"
 
-%w(Music Pictures Public Templates Video).each { |d| system "rm -rf ~/#{d}" }
-
 system "ruby ~/bin/gconf.rb"
+
+%w(Music Examples Desktop Pictures Public Templates Videos Documents).each { |d| system "rm -rf ~/#{d}" }
