d6f39da83961e3a5989522a7c0a9ed6af2f6589c
Phil Hagelberg <technomancy@gmail.com>
2008-07-07 12:18:02 -0700

    whitespace cleanup and font additions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -59,11 +59,11 @@
 
 (let ((autoload-file "~/.emacs.d/loaddefs.el"))
   (if (or (not (file-exists-p autoload-file))
-	  (< (+ (car (nth 5 (file-attributes autoload-file))) 20)
-	     (car (current-time))))
+          (< (+ (car (nth 5 (file-attributes autoload-file))) 20)
+             (car (current-time))))
       (let ((generated-autoload-file autoload-file))
-	(message "Updating autoloads...")
-	(update-directory-autoloads "~/.emacs.d/")))
+        (message "Updating autoloads...")
+        (update-directory-autoloads "~/.emacs.d/")))
   (load autoload-file))
 
 (autoload 'nxhtml-mode "nxml/autostart" "" t)
@@ -99,7 +99,8 @@
   '(progn
      (require 'log-view)
      (define-key log-view-mode-map (kbd "RET") 'log-view-find-revision)
-     (define-key vc-annotate-mode-map (kbd "RET") 'vc-annotate-find-revision-at-line)))
+     (define-key vc-annotate-mode-map (kbd "RET")
+       'vc-annotate-find-revision-at-line)))
 
 (ignore-errors
   (load "elpa/package.el")
@@ -133,7 +134,7 @@
 
 (setq system-specific-config
       (concat "~/.emacs.d/"
-	      (substring (shell-command-to-string "hostname") 0 -1) ".el"))
+              (substring (shell-command-to-string "hostname") 0 -1) ".el"))
 
 (if (file-exists-p system-specific-config)
     (load system-specific-config))
@@ -227,7 +228,7 @@
 
 ;; Make my face customizations stick around with other color themes.
 
-;; Look into zeroconf-chat over dbus
+;; Look into adding zeroconf-chat to jabber.el
 
 ;;; Minor
 
--- .emacs.d/my-misc.el
+++ .emacs.d/my-misc.el
@@ -50,7 +50,7 @@
   (setq ido-enable-prefix nil
         ido-enable-flex-matching t
         ido-create-new-buffer 'always
-	ido-use-filename-at-point t
+        ido-use-filename-at-point t
         ido-max-prospects 10))
 
 (put 'narrow-to-region 'disabled nil)
