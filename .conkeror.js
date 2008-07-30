b129ed1c01bd55a6c0f11b011947bdf87881f8d5
Phil Hagelberg <technomancy@gmail.com>
2008-07-30 09:57:53 -0700

    some code inspection tools

--- .conkeror.js
+++ .conkeror.js
@@ -23,3 +23,45 @@ can_kill_last_buffer = false;
 register_user_stylesheet( 
     "data:text/css,"+
         escape("#minibuffer, tree.completions, .mode-line { font-family: Terminus; }"));
+
+/* Sets up the reddit-mode for the given buffer. */
+function twitter_mode_setup(buffer) {
+    var blacklist = /iphone|twitteriffic/i;
+    var document = buffer.document;
+    if(document.twitter) return;
+    else document.twitter_mode_loaded = true;
+    if(!String(window.content.location).substr(0, 23) == "http://twitter.com/home") return;
+
+    var trs = document.getElementsByTagName("tr");
+    for each(let tr in trs) {
+        if(tr.innerHTML.match(blacklist)) {
+            tr.style.display = "none";
+        };
+    };
+}
+
+/* Setting up and tearing down the mode */
+
+function enable_twitter_mode(buffer) {
+  var doc = buffer.document;
+  if(doc.twitterCurrent != null)
+    twitter_highlight(doc.twitterLinkDivs[doc.twitterCurrent]);
+  buffer.local_variables.content_buffer_normal_keymap = twitter_keymap;
+  add_hook.call(buffer, "content_buffer_finished_loading_hook", twitter_mode_setup);
+}
+
+function disable_twitter_mode(buffer) {
+  var doc = buffer.document;
+
+  if(doc.twitterCurrent != null)
+    twitter_dehighlight(doc.twitterLinkDivs[doc.twitterCurrent]);
+  remove_hook.call(buffer, "content_buffer_finished_loading_hook", twitter_mode_setup);
+}
+
+define_page_mode("twitter_mode", "twitter",
+                 $enable = enable_twitter_mode,
+                 $disable = disable_twitter_mode,
+                 $doc = "twitter page-mode: blacklisting for twitter.");
+
+var twitter_re = build_url_regex($domain = /([a-zA-Z0-9\-]*\.)*twitter/);
+auto_mode_list.push([twitter_re, twitter_mode]);
