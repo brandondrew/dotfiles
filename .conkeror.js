f3f9db60a7dbd30b8fce81e8d21173f1a18c48c6
Phil Hagelberg <technomancy@gmail.com>
2008-09-22 14:48:32 -0700

    Fix Conkeror dotfiles; temporarily disable nxml-mode.

--- .conkeror.js
+++ .conkeror.js
@@ -2,18 +2,21 @@
 // - js2-mode
 // - monospace font
 
-interactive("fill_domain", function (I) {
+interactive("fill-domain", "Fill the minibuffer with the current domain.",
+  function (I) {
 	      var field = I.minibuffer.input_element;
 	      var paths = String(I.window.content.location).split('/');
 	      var domain = paths[0] + "/" + paths[1] + "/" + paths[2] + "/";
-	      field.value = domain;});
+	      field.value = domain;
+});
 
-interactive("tinyurl", function(I) {
+interactive("tinyurl", "Create a TinyURL for the current URL",
+  function(I) {
     I.window.content.location.href = 'http://tinyurl.com/create.php?url=' +
         encodeURIComponent(I.window.content.location.href);
 });
 
-define_key(minibuffer_keymap, kbd("/", MOD_CTRL), "fill_domain");
+define_key(minibuffer_keymap, kbd("/", MOD_CTRL), "fill-domain");
 
 add_webjump("hub", "http://github.com/search?q=%s");
 add_webjump("wikipedia", "http://www.google.com/search?q=wikipedia+%s&btnI=I'm Feeling Lucky");
@@ -22,7 +25,6 @@ add_delicious_webjumps ("technomancy");
 url_remoting_fn = load_url_in_new_buffer;
 url_completion_use_history = true;
 can_kill_last_buffer = false;
-
 
 register_user_stylesheet(
     "data:text/css,"+
