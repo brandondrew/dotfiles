b5fa0349dfa0a1b69cbcd5c9173eaf3f3b9eac1c
Phil Hagelberg <technomancy@gmail.com>
2008-12-04 11:37:20 -0800

    remove a file that snuck in

--- .emacs.d/my-ruby.el
+++ .emacs.d/my-ruby.el
@@ -62,7 +62,7 @@
 ;;
 
 (define-key ruby-mode-map "\C-\M-h" 'backward-kill-word)
-(define-key ruby-mode-map (kbd "RET") 'ruby-reindent-then-newline-and-indent)
+(define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent)
 (define-key ruby-mode-map (kbd "C-c l") (lambda ()
                                           (interactive) (insert "lambda")))
 (define-key ruby-mode-map (kbd "C-\\") 'rct-complete-symbol)
